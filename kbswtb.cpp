7abdabd9bc4699405b870a75a443ed67d7c41676
donaastor <45340801+donaastor@users.noreply.github.com>
2022-08-08 06:08:42 +0200

    Update kbswtb.cpp

--- kbswtb.cpp
+++ kbswtb.cpp
@@ -6,29 +6,20 @@
 int main(){
 	
 	struct stat buffer;
-	int postojanje=stat("/tmp/i3statusP",&buffer);
+	int postojanje = stat("/tmp/i3statusP",&buffer);
 	if(postojanje!=0)
 		mkfifo("/tmp/i3statusP",0666);
-	int fdw=open("/tmp/i3statusP",O_RDWR|O_NONBLOCK);
-	const char *nru="U\n";
+	int fdw = open("/tmp/i3statusP",O_RDWR|O_NONBLOCK);
+	const char *nru = "U\n";
 	
-	char displayName[2048]={0};
-	int eventCode;
-	int errorReturn;
-	int major = XkbMajorVersion;
-	int minor = XkbMinorVersion;
-	int reasonReturn;
-	Display* _display=
-		XkbOpenDisplay(displayName, &eventCode, &errorReturn,
-			&major, &minor, &reasonReturn);
+	char displayName[2048] = {0};
+	Display *_display = XOpenDisplay(displayName);
 	
-	XkbSelectEventDetails(_display, XkbUseCoreKbd, XkbStateNotify,
-		XkbAllStateComponentsMask, XkbGroupStateMask);
+	XkbSelectEventDetails(_display, XkbUseCoreKbd, XkbStateNotify, XkbAllStateComponentsMask, XkbGroupStateMask);
 	
 	while(true){
 		XEvent event;
 		XNextEvent(_display, &event);
-		
 		write(fdw,nru,2);
 	}
 	
