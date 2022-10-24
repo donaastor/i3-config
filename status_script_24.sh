ba4ed3e00cc75617631ae7bcaa90b0ca03713515
donaastor <45340801+donaastor@users.noreply.github.com>
2022-10-24 14:07:35 +0200

    Update status_script_24.sh

--- status_script_24.sh
+++ status_script_24.sh
@@ -9,7 +9,7 @@ if ! [ -e $thc ]; then
   for i in {0..3}; do
     if [ -e $cl0$i$cl1 ]; then
       if [ $cl0$i$cl1 = Tccd1 ]; then
-	ln -s $cl0$i $thc
+        ln -s $cl0$i $thc
         break
       fi
     fi
